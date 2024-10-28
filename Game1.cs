87d3166f3a13ade4da930736d28d6893e127e852
fixed0000 <ssh2112@yandex.ru>
2024-10-28 15:45:36 +0300

    monogame_project

---
+++ Game1.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace coding;
+
+public class Game1 : Game
+{
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
+    int deadZone;
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 4,
+                                   _graphics.PreferredBackBufferHeight / 4);
+        ballSpeed = 100f;
+        deadZone = 4096;
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        ballTexture = Content.Load<Texture2D>("ball");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+ // The time since Update was called last.
+        float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        var kstate = Keyboard.GetState();
+        
+        if (kstate.IsKeyDown(Keys.Up))
+        {
+            ballPosition.Y -= updatedBallSpeed;
+        }
+        
+        if (kstate.IsKeyDown(Keys.Down))
+        {
+            ballPosition.Y += updatedBallSpeed;
+        }
+        
+        if (kstate.IsKeyDown(Keys.Left))
+        {
+            ballPosition.X -= updatedBallSpeed;
+        }
+        
+        if (kstate.IsKeyDown(Keys.Right))
+        {
+            ballPosition.X += updatedBallSpeed;
+        }
+
+        if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+        {
+            ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+        }
+        else if (ballPosition.X < ballTexture.Width / 8)
+        {
+            ballPosition.X = ballTexture.Width / 8;
+        }
+
+        if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+        {
+            ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+        }
+        else if (ballPosition.Y < ballTexture.Height / 30)
+        {
+            ballPosition.Y = ballTexture.Height / 30;
+        }
+
+        if(Joystick.LastConnectedIndex == 0)
+        {
+        JoystickState jstate = Joystick.GetState((int) PlayerIndex.One);
+
+        if (jstate.Axes[1] < -deadZone)
+        {
+            ballPosition.Y -= updatedBallSpeed;
+        }
+        else if (jstate.Axes[1] > deadZone)
+        {
+            ballPosition.Y += updatedBallSpeed;
+        }
+
+        if (jstate.Axes[0] < -deadZone)
+        {
+            ballPosition.X -= updatedBallSpeed;
+        }
+        else if (jstate.Axes[0] > deadZone)
+        {
+            ballPosition.X += updatedBallSpeed;
+        }
+        }
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+           _spriteBatch.Begin();
+        _spriteBatch.Draw(ballTexture, ballPosition, Color.White);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ check.py
@@ -0,0 +1,33 @@
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class FileChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path == file_path:
+            print(f"Файл '{file_path}' был изменен! Запуск 'dotnet build'...")
+            # Выполняем команду dotnet build
+            result = subprocess.run(["dotnet", "build"], capture_output=True, text=True)
+            
+            # Выводим результат выполнения команды
+            print(result.stdout)
+            if result.returncode != 0:
+                print("Произошла ошибка при выполнении 'dotnet build':")
+                print(result.stderr)
+
+file_path = 'C:\coding\'
+event_handler = FileChangeHandler()
+observer = Observer()
+observer.schedule(event_handler, path=file_path, recursive=False)
+
+print(f"Отслеживание изменений в файле: {file_path}")
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
